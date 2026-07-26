4a258ff2d124174838f3bdd58d81d24275390cbf
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-07-26 14:52:31 +0900

    Time: 0 ms (100.00%) | Memory: 19.6 MB (53.53%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        if len(mat) == 1:
+            return mat[0][0]
+
+        # 斜めの数字の合計を足す
+        sum = 0
+        for i in range(len(mat)):
+            # 左上から右下にかけて
+            sum += mat[i][i]
+            # 右上から左下にかけて
+            sum += mat[i][len(mat)-i-1]
+        
+
+        # 行のサイズが奇数の場合、中心部分の数字が2回カウントされているので、1回分カウントを減らす
+        if len(mat) % 2 == 1:
+            center_point = len(mat) // 2
+            center_num = mat[center_point][center_point]
+            sum -= center_num
+
+        return sum
