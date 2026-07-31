f5b6f01527c94b7635a8ed8c9eac95fa58049d88
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-07-31 21:51:19 +0900

    Time: 1 ms (44.92%) | Memory: 19.3 MB (24.34%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,26 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 各開きカッコの数をカウントする
-        count_stack = []
+        # 開きカッコをスタックしていく
+        bracket_stack = []
+        open_brackets = ["(", "{", "["]
+        bracket_pairs = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
 
-        for char in s:
-            match char:
-                case "(" | "{" | "[":
-                    count_stack.append(char)
-                case ")":
-                    if not count_stack or count_stack[-1] != "(":
-                        return False
-                    else:
-                        count_stack.pop()
-                case "}":
-                    if not count_stack or count_stack[-1] != "{":
-                        return False
-                    else:
-                        count_stack.pop()
-                case "]":
-                    if not count_stack or count_stack[-1] != "[":
-                        return False
-                    else:
-                        count_stack.pop()
+        for bracket in s:
+            if bracket in open_brackets:
+                bracket_stack.append(bracket)
+            else:
+                if not bracket_stack or bracket_stack[-1] != bracket_pairs[bracket]:
+                    return False
+                bracket_stack.pop()
 
-        return len(count_stack) == 0
+        return len(bracket_stack) == 0
