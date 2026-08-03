0b59a0703c49ee2907b57219413aa214e83da942
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-03 20:31:39 +0900

    Time: 16 ms (26.66%) | Memory: 24.1 MB (9.01%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        counter_list = defaultdict(list)
-        for value in strs:
-            # 小文字英字の各文字の出現回数のカウンター
+        groups = defaultdict(list)
+
+        for st in strs:
             counter = [0] * 26
-            for s in value:
-                counter[ord(s) - ord("a")] += 1
+            for c in st:
+                counter[ord(c) - ord("a")] += 1
 
-            counter_list[tuple(counter)].append(value)
-
-        return list(counter_list.values())
-
-
-
+            groups[tuple(counter)].append(st)
         
+        return list(groups.values())
