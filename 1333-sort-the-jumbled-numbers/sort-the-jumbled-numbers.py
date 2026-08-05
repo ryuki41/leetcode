242152d7f23b46597477eca5382831de8f9cc21d
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-05 22:17:58 +0900

    Time: 508 ms (52.00%) | Memory: 26.4 MB (68.00%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,23 +1,14 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        def num_mapping(num: int):
-            if num == 0:
-                return mapping[num]
+        def compare(num: int):
+            new_st_num = ""
+            st_num = str(num)
             
+            for st in st_num:
+                new_st_num += str(mapping[int(st)])
+            
+            return int(new_st_num)
 
-            digit = 1
-            mapped_num = 0
-            while num > 0:
-                # 1桁ずつ数字を見て、マッピング後の値に変換する
-                digit_num = num % 10
-                mapped_num += mapping[digit_num] * digit
-
-                num = num // 10
-                digit *= 10
-
-            return mapped_num
-
-
-        nums.sort(key=num_mapping)
-
-        return nums
+        return sorted(nums, key=compare)
+            
+        
