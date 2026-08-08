4c48ed72c9233db97ba8dcb403f7a802dd964de9
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-08 18:19:33 +0900

    Time: 0 ms (100.00%) | Memory: 19.4 MB (25.47%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -3,13 +3,13 @@ class Solution:
         s_pointer = 0
 
         if len(s) == 0:
-            return  True
-
-        for t_pointer in range(len(t)):
-            if s[s_pointer] == t[t_pointer]:
+            return True
+            
+        for i in range(len(t)):
+            if t[i] == s[s_pointer]:
                 s_pointer += 1
                 if s_pointer == len(s):
-                    # sの全ての文字の並び順が担保されている
                     return True
-
-        return False
+            
+        return s_pointer == len(s)
+            
