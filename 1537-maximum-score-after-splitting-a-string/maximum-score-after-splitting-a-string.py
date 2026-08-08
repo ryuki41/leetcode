5cfc816fbe5187171dd9eb950497d5fe009ebb9f
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-08 17:33:33 +0900

    Time: 3 ms (38.52%) | Memory: 19.2 MB (88.90%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,20 +1,23 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        max_sum = 0
-
-        for i in range(1, len(s)):
-            num_sum = 0
-
-            # leftの0の個数カウント
-            for j in range(i):
-                if s[j] == "0":
-                    num_sum += 1
-
-            # rightの1の個数カウント 
-            for k in range(i, len(s)):
-                if s[k] == "1":
-                    num_sum += 1
-
-            max_sum = max(max_sum, num_sum)
+        # rightになるscoreを数える
+        right_score = 0
+        for i in range(len(s)):
+            if s[i] == "1":
+                right_score += 1
         
-        return max_sum
+        left_score = 0
+        max_score = 0
+        # 1つずつleftに移していきscoreをカウントする
+        for i in range(len(s)-1):
+            if s[i] == "0":
+                left_score += 1
+            else:
+                right_score -= 1    
+            score = left_score + right_score
+            max_score = max(max_score, score)
+        
+        return max_score
+            
+
+
