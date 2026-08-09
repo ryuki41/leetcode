6ce2ea3bd95a9d39e3e09656d19449be4e0faa69
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-09 14:11:05 +0900

    Time: 1 ms (10.47%) | Memory: 19.4 MB (67.35%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,16 +1,25 @@
+from copy import deepcopy as cp
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        N = len(nums)
 
         res = []
-        for i in range(2**len(nums)):
-            # 2進数にしてゼロ埋め
-            binary_num = format(i, f"0{len(nums)}b")
 
-            res_elm = []
-            for j in range(len(binary_num)):
-                if binary_num[j] == "1":
-                    res_elm.append(nums[j])
+        subset = []
+
+        def rec(i):
+            if i == N:
+                res.append(cp(subset))
+                return
             
-            res.append(res_elm)
-        
+            subset.append(nums[i])
+            rec(i+1)
+
+            subset.pop()
+            rec(i+1)
+
+            return
+
+        rec(0)
         return res
