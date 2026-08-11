3f972701a2273a496783c35a09f7d7f041c87717
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-11 13:31:24 +0900

    Time: 11 ms (77.42%) | Memory: 19.3 MB (75.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,16 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == 0 and len(t) == 0:
-            return True
-
-        if len(t) != len(s):
+        if len(s) != len(t):
             return False
 
-        s_word_count = defaultdict(int)
-        t_word_count = defaultdict(int)
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
 
         for i in range(len(s)):
-            s_word_count[s[i]] += 1
-            t_word_count[t[i]] += 1
+            s_dict[s[i]] += 1
+            t_dict[t[i]] += 1
         
-        return s_word_count == t_word_count
+        return s_dict == t_dict
