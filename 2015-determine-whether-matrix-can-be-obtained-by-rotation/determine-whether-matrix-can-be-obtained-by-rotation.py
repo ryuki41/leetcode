aad19b8a22230e452b8240810bb9c04d43f744a6
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-11 14:53:47 +0900

    Time: 3 ms (11.24%) | Memory: 19.3 MB (26.34%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,21 +1,20 @@
 class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        all_rotation = [True] * 4
-
         n = len(mat)
+        degree_result = [True] * 4
         for i in range(n):
             for j in range(n):
                 # 回転しない場合
                 if mat[i][j] != target[i][j]:
-                    all_rotation[0] = False
+                    degree_result[0] = False
                 # 90度回転する場合
                 if mat[i][j] != target[j][n-i-1]:
-                    all_rotation[1] = False
+                    degree_result[1] = False
                 # 180度回転する場合
-                if mat[i][j] != target[n-1-i][n-1-j]:
-                    all_rotation[2] = False
-                # 270度回転する場合   
+                if mat[i][j] != target[n-i-1][n-j-1]:
+                    degree_result[2] = False
+                # 270度回転する場合
                 if mat[i][j] != target[n-j-1][i]:
-                    all_rotation[3] = False
-        
-        return any(all_rotation)
+                    degree_result[3] = False
+
+        return any(degree_result)
