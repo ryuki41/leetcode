0211b10d2cc1f78089e2ea8cb21046df7febf5f1
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-25 19:45:40 +0900

    Time: 46 ms (60.30%) | Memory: 28.7 MB (20.82%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        # n日目以降の最高株価
-        max_prices = [0] * n
-        max_prices[n-1] = prices[n-1]
+        buy_price = prices[0]
+        profit = 0
 
-        for i in reversed(range(1, n-1)):
-            max_prices[i] = max(prices[i], max_prices[i+1])
+        for i in range(1,len(prices)):
+            p = prices[i]
+
+            if buy_price > p:
+                buy_price = p
+            
+            profit = max(profit, p - buy_price)
         
-        max_profit = 0
-        print(max_prices)
-        for i in range(n-1):
-            cur_max_profit = max_prices[i+1] - prices[i]
-            max_profit = max(max_profit, cur_max_profit)
-        
-        return max_profit
+        return profit
