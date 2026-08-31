9418032e75e1acf8974c496da231123404eaab63
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-08-31 21:39:25 +0900

    Time: 19 ms (64.15%) | Memory: 19.8 MB (8.75%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,14 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        m_list = [0] * 26
+        note, mag = Counter(ransomNote), Counter(magazine)
 
-        for i in range(len(magazine)):
-            m_list[ord(magazine[i]) - ord("a")] += 1
-        
-        for i in range(len(ransomNote)):
-            if m_list[ord(ransomNote[i]) - ord("a")] == 0:
-                return False
-            m_list[ord(ransomNote[i]) - ord("a")] -= 1
-        
-        return True
-
+        if note & mag == note:
+            return True
+        return False
