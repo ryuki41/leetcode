649df3008e0b607ac0e659bb8ee8c14eb944f0cc
ryuki41 <87225756+ryuki41@users.noreply.github.com>
2026-09-10 21:08:26 +0900

    Time: 27 ms (5.21%) | Memory: 46.4 MB (33.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,16 +4,15 @@ class Solution {
             return false;
         }
 
-        Map<Character, Integer> s_count = new HashMap<>();
-        Map<Character, Integer> t_count = new HashMap<>();
+        Map<Character, Integer> s_map = new HashMap<>();
+        Map<Character, Integer> t_map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            int tmp_s_count = s_count.getOrDefault(s.charAt(i), 0);
-            s_count.put(s.charAt(i), tmp_s_count+1);
-            int tmp_t_count = t_count.getOrDefault(t.charAt(i), 0);
-            t_count.put(t.charAt(i), tmp_t_count+1);
+            s_map.put(s.charAt(i), s_map.getOrDefault(s.charAt(i), 0) + 1);
+            t_map.put(t.charAt(i), t_map.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        return s_count.equals(t_count);
+
+        return s_map.equals(t_map);
     }
 }
